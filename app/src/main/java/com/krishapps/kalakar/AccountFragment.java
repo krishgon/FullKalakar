63d15f97cc6e3e2d82db8066cae09881c08a27d2
krishgon <krishgondia@gmail.com>
2021-05-31 09:31:56 +0530

    add functionality to fetch user details from firebase and update the account page accordingly

--- app/src/main/java/com/krishapps/kalakar/AccountFragment.java
+++ app/src/main/java/com/krishapps/kalakar/AccountFragment.java
@@ -2,36 +2,76 @@ package com.krishapps.kalakar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class AccountFragment  extends Fragment {
     public AccountFragment(){
         super(R.layout.account_fragment);
     }
 
-    private Button logOut_button;
+    Button logOut_button;
+    TextView user_name_textView, user_userName_textView, userMail_textView, userPhNum_textView;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firestore;
+    String userID;
+    DocumentReference documentReference;
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        logOut_button = view.findViewById(R.id.logOut_button_original);
 
-        // code to logout the user when clicked on log out button
-        logOut_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
+        Log.d("krishlog", "onViewCreated: reached here in this fragment");
 
-                Intent intent = new Intent(getActivity(), Authentication.class);
-                startActivity(intent);
-            }
-        });
+        // collect the ui elements
+            logOut_button = view.findViewById(R.id.logOut_button_original);
+            user_name_textView = view.findViewById(R.id.userName_textView);
+            user_userName_textView = view.findViewById(R.id.userUserName_textView);
+            userMail_textView = view.findViewById(R.id.userMail_textView);
+            userPhNum_textView = view.findViewById(R.id.userPhNum_textView);
+
+        // collect the data of signed user
+            firebaseAuth = FirebaseAuth.getInstance();
+            firestore = FirebaseFirestore.getInstance();
+
+            userID = firebaseAuth.getCurrentUser().getUid();
+
+            documentReference = firestore.collection("users").document(userID);
+            documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                    userPhNum_textView.setText(documentSnapshot.getString("phoneNumber"));
+                    user_name_textView.setText(documentSnapshot.getString("fullName"));
+                    user_userName_textView.setText(documentSnapshot.getString("userName"));
+                    userMail_textView.setVisibility(View.INVISIBLE);
+                }
+            });
+
+
+
+        // logout the user when clicked on log out button
+            logOut_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FirebaseAuth.getInstance().signOut();
+
+                    Intent intent = new Intent(getActivity(), Authentication.class);
+                    startActivity(intent);
+                }
+            });
 
     }
 }
--- app/src/main/java/com/krishapps/kalakar/UserDetailsFragment.java
+++ app/src/main/java/com/krishapps/kalakar/UserDetailsFragment.java
@@ -38,102 +38,101 @@ public class UserDetailsFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        // assign ui elements to their programatic variables
-        userName_textInputLayout = view.findViewById(R.id.userName_outlinedTextField);
-        name_textInputLayout = view.findViewById(R.id.name_outlinedTextField);
-        userName_editText = userName_textInputLayout.getEditText();
-        name_editText = name_textInputLayout.getEditText();
-        Button submit_button = view.findViewById(R.id.submit_button);
+        // collect ui elements
+            userName_textInputLayout = view.findViewById(R.id.userName_outlinedTextField);
+            name_textInputLayout = view.findViewById(R.id.name_outlinedTextField);
+            userName_editText = userName_textInputLayout.getEditText();
+            name_editText = name_textInputLayout.getEditText();
+            Button submit_button = view.findViewById(R.id.submit_button);
 
         //TODO: do something to not repeat the code below for disabling the animation of text field
 
         // disable the animation of label/hint of material design's text field
-        userName_textInputLayout.setHint(null);
-        userName_editText.setHint("username");
-        name_textInputLayout.setHint(null);
-        name_editText.setHint("full name");
+            userName_textInputLayout.setHint(null);
+            userName_editText.setHint("username");
+            name_textInputLayout.setHint(null);
+            name_editText.setHint("full name");
 
         // get the current user's id and phone number
-        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        phoneNumber = FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber();
-
-        firestore = FirebaseFirestore.getInstance();
+            userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            phoneNumber = FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber();
 
         // register user in firebase when clicked on submit button
-        submit_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(areUserDetailsValid()){
-                    fullName = name_editText.getText().toString();
-                    userName = userName_editText.getText().toString().trim();
-                    Log.d("krishlog", "onClick: the number probably is " + FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber());
+            firestore = FirebaseFirestore.getInstance();
 
-                    DocumentReference documentReference = firestore.collection("users").document(userID);
+            submit_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(areUserDetailsValid()){
+                        fullName = name_editText.getText().toString();
+                        userName = userName_editText.getText().toString().trim();
+                        Log.d("krishlog", "onClick: the number probably is " + FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber());
 
-                    HashMap<String, Object> user = new HashMap<>();
-                    user.put("fullName", fullName);
-                    user.put("phoneNumber", phoneNumber);
-                    user.put("userName", userName);
+                        DocumentReference documentReference = firestore.collection("users").document(userID);
 
-                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Log.d("krishlog", "onSuccess: user profile is completed");
-                            startActivity(new Intent(getActivity(), MainActivity.class));
-                            getActivity().finish();
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d("krishlog", "onFailure: " + e.getMessage());
-                        }
-                    });
+                        HashMap<String, Object> user = new HashMap<>();
+                        user.put("fullName", fullName);
+                        user.put("phoneNumber", phoneNumber);
+                        user.put("userName", userName);
+
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d("krishlog", "onSuccess: user profile is completed");
+                                startActivity(new Intent(getActivity(), MainActivity.class));
+                                getActivity().finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("krishlog", "onFailure: " + e.getMessage());
+                            }
+                        });
+                    }
                 }
-            }
-        });
+            });
 
         // set text watchers on edit text for errors to be cleared dynamically
-        name_editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            name_editText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if(s.toString().isEmpty() == false){
-                    name_textInputLayout.setError(null);
-                }else {
-                    name_textInputLayout.setError("Required");
                 }
-            }
-        });
-        userName_editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            }
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if(s.toString().isEmpty() == false){
-                    userName_textInputLayout.setError(null);
-                }else {
-                    userName_textInputLayout.setError("Required");
                 }
-            }
-        });
 
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if(s.toString().isEmpty() == false){
+                        name_textInputLayout.setError(null);
+                    }else {
+                        name_textInputLayout.setError("Required");
+                    }
+                }
+            });
+            userName_editText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    if(s.toString().isEmpty() == false){
+                        userName_textInputLayout.setError(null);
+                    }else {
+                        userName_textInputLayout.setError("Required");
+                    }
+                }
+            });
 
     }
 
