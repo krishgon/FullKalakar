16300f0291ab47f7e768f7e7bce440c460fdf426
krishgon <krishgondia@gmail.com>
2021-06-03 06:50:01 +0530

    add layout for editing user profile

---
+++ app/src/main/java/com/krishapps/kalakar/MainFragments/EditProfileFragment.java
@@ -0,0 +1,11 @@
+package com.krishapps.kalakar.MainFragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.krishapps.kalakar.R;
+
+public class EditProfileFragment extends Fragment {
+    public EditProfileFragment(){
+        super(R.layout.edit_profile_fragment);
+    }
+}
